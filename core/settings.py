f6d7a9e52c3a7813ee89e514322f46a06f8a1774
Kristian Cross <etamin1985@yahoo.com>
2026-01-07 17:06:32 +0000

    Relete reviews login middleware (previous project) from settings.

--- core/settings.py
+++ core/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'axes.middleware.AxesMiddleware',
-    'reviews.login_middleware.LoginRequiredMiddleware',
 ]
 
 # Templates
