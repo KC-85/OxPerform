cd4f139766ee27b40bca5dea092fe9541f6a68bc
Kristian Cross <etamin1985@yahoo.com>
2026-01-08 13:54:10 +0000

    Create apps.py for Oxford city

---
+++ apps/events/oxford/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class OxfordCityConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.events.oxford"
+    label = "oxfordcity"
+    verbose_name = "Oxford"
