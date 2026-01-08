0aa2b2bf63c55a5c7343191294c003e6e0f6f670
Kristian Cross <etamin1985@yahoo.com>
2026-01-08 16:02:10 +0000

    Create apps.py for South and West Oxfordshire

---
+++ apps/events/oxfordshire/south/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SouthOxfordshireConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.events.oxfordshire.south"
+    label = "southoxon"
+    verbose_name = "Oxfordshire (South)"
---
+++ apps/events/oxfordshire/west/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class WestOxfordshireConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.events.oxfordshire.west"
+    label = "westoxon"
+    verbose_name = "Oxfordshire (West)"
