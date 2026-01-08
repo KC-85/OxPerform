1e7cae0bb317681acdb9dd4d38ef589c8255f889
Kristian Cross <etamin1985@yahoo.com>
2026-01-08 14:28:21 +0000

    Create apps.py for North Oxfordshire

---
+++ apps/events/oxfordshire/north/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class NorthOxfordshireConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.events.oxfordshire.north"
+    label = "northoxon"
+    verbose_name = "Oxfordshire (North)"
