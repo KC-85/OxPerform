f42e081bae7e4d543ad72c2516e822edbccae176
Kristian Cross <etamin1985@yahoo.com>
2026-01-12 20:10:51 +0000

    Add urlpatterns for south oxfordshire

---
+++ apps/events/oxfordshire/south/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+app_name = "southoxon"
+
+urlpatterns = [
+    # Events
+    path("", views.upcoming_events, name="upcoming_events"),
+    path("past/", views.past_events, name="past_events"),
+    path("category/<slug:category>/", views.category_events, name="category_events"),
+
+    # Venues
+    path("venues/", views.venue_list, name="venue_list"),
+    path("venues/<int:pk>/", views.venue_detail, name="venue_detail"),
+
+    # Event Detail
+    path("<slug:slug>/", views.event_detail, name="event_detail"),
+]
